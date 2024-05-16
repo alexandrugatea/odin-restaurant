ea4d8dda7bb42a8dfa5941e21c018b1645c1e26a
Alexandru Gatea <alexandru.gatea@gmail.com>
2024-05-16 23:24:50 +0300

    changed port, upload build folder

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         static: {
             directory: path.join(__dirname, 'build'),
         },
-        port: 3000,
+        port: 3399,
         open: true,
     },
     module: {
